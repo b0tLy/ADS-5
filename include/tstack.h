f56d6d03a519ce992918d21e189c20879c7f81ce
Stepa Shishkin <151217141+b0tLy@users.noreply.github.com>
2024-04-14 18:09:42 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,40 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+  T data[300];
+  int top;
+
+ public:
+  TStack() : top(0) {}
+
+  void push(const T &item) {
+    if (top <= size - 1) {
+      data[top++] = item;
+    } else {
+      throw std::string("Стек переполнен");
+    }
+  }
+
+  T pop() {
+    if (top > 0) {
+      return data[--top];
+    } else {
+      throw std::string("Стек пуст");
+    }
+  }
+
+  T get() const {
+    if (top > 0) {
+      return data[top - 1];
+    } else {
+      throw std::string("Пусто");
+    }
+  }
+
+  bool isEmpty() const {
+    return top == 0;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
